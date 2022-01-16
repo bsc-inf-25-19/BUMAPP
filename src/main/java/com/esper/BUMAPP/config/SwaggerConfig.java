83a2cf5352988bb4032ef2ec8bad933387de5434
JOEL JONES <bsc-inf-25-19@unima.ac.mw>
2022-01-16 17:46:23 +0200

    added config and swagger documentation

---
+++ src/main/java/com/esper/BUMAPP/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.esper.BUMAPP.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.esper.BUMAPP"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        Contact contact = new Contact("BSC-INF-25-19", "http://esper.bankai.com",
+                "bsc-inf-25-19@unima.mw");
+        return new ApiInfoBuilder()
+                .title("BUMAPP API")
+                .description("Documentation BUMAPP api")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build();
+    }
+}
